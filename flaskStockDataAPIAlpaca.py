160f415093fa827fe2f589de35fe4b6ac94f4526
shahryaradil <shahryar_adil10@hotmail.com>
2023-07-11 15:30:10 +0500

    Update flaskStockDataAPIAlpaca.py
    Added adjusted price to the returning data frame

--- flaskStockDataAPIAlpaca.py
+++ flaskStockDataAPIAlpaca.py
@@ -12,26 +12,50 @@ def get_stock_data():
     ticker = request.args.get('ticker')
     date_string = request.args.get('date')
 
-    # parsing date string to datetime type
     date = datetime.strptime(date_string, '%d/%m/%Y').date()
     start_date = datetime(date.year, date.month, date.day)
     end_date = datetime.now().date()
 
+    api_key = ''
+    api_secret = ''
+
     client = StockHistoricalDataClient(api_key, api_secret)
-    request_params = StockBarsRequest(
+
+    # Fetch adjusted data
+    adjusted_request_params = StockBarsRequest(
+        symbol_or_symbols=[ticker],
+        timeframe=TimeFrame.Day,
+        start=start_date.strftime('%Y-%m-%d %H:%M:%S'),
+        end=end_date.strftime('%Y-%m-%d %H:%M:%S'),
+        adjustment='all'
+    )
+    adjusted_bars = client.get_stock_bars(adjusted_request_params)
+    adjusted_bars_df = adjusted_bars.df
+
+    # Fetch unadjusted data
+    unadjusted_request_params = StockBarsRequest(
         symbol_or_symbols=[ticker],
         timeframe=TimeFrame.Day,
         start=start_date.strftime('%Y-%m-%d %H:%M:%S'),
         end=end_date.strftime('%Y-%m-%d %H:%M:%S')
     )
-    bars = client.get_stock_bars(request_params)
-    
-    bars_df = bars.df
+    unadjusted_bars = client.get_stock_bars(unadjusted_request_params)
+    unadjusted_bars_df = unadjusted_bars.df
 
-    # Convert DataFrame to JSON
-    date_filter_json = bars_df.reset_index(drop=True).to_json(orient='records')
+    # Rename adjusted columns
+    adjusted_bars_df.rename(columns={
+        'open': 'adjusted_open',
+        'close': 'adjusted_close',
+        'high': 'adjusted_high',
+        'low': 'adjusted_low'
+    }, inplace=True)
 
-    return jsonify(date_filter_json)
+    # Combine adjusted and unadjusted dataframes
+    combined_df = pd.concat([unadjusted_bars_df, adjusted_bars_df[['adjusted_open', 'adjusted_close', 'adjusted_high', 'adjusted_low']]], axis=1)
+
+    combined_json = combined_df.reset_index(drop=True).to_json(orient='records')
+
+    return jsonify(combined_json)
 
 if __name__ == '__main__':
     app.run()
